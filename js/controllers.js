b0757318a9f23a6d0d1c0e7f79dcd5b436af9336
John Ward <ward.johnm@gmail.com>
2015-07-01 11:12:16 -0700

    built image tiles

--- js/controllers.js
+++ js/controllers.js
@@ -4,39 +4,20 @@ angular
 	function JamController($http) {
 	    var self = this;
 	    self.foo = "bar";
+	    self.searchInstagram = searchInstagram('', function(data) {
+			                    self.pictures = data.data;
+			                });
       
   	
-	function searchInstagram(tag){
+	function searchInstagram(name,callback){
 		var method = 'GET'
-		var url = 'https://api.instagram.com/v1/media/search'
-		var params = {
-			
+		var url = 'https://api.instagram.com/v1/tags/birthday/media/recent?access_token=45152981.1fb234f.f715923e64d843deb89a3c2deb3a6601&callback=JSON_CALLBACK'
+		// var params = {	
+		// 	access_token: '45152981.1fb234f.f715923e64d843deb89a3c2deb3a6601' 
+		// }
+			$http.jsonp(url).success(callback);
 		}
+	
 	}
 
-
-
-
-  	function returnYelp(name,callback) {
-                        var method = 'GET';
-                        var url = 'http://api.yelp.com/v2/search';
-                        var params = {
-                                callback: 'angular.callbacks._0',
-                                location: $scope.trip.location,
-                                oauth_consumer_key: '028e6Wxp-UzuX6K2jc3ZPA', 
-                                oauth_nonce: randomString(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'),
-                                oauth_signature_method: "HMAC-SHA1",
-                                oauth_token: 'UXqJ2CyAaKQD_mzqH8mMp7Y7-lIrWhqt', //Token
-                                oauth_timestamp: new Date().getTime(),
-                                term: 'attractions',
-                                limit: 20           
-                        };
-                        var consumerSecret = '5h_r39wNZo5GMcEGIKbaZ18busA'; 
-                        var tokenSecret = 'oKSqk3Kpc3QMdPsLtHMZnzRqH-0'; //Token Secret
-                        var signature = oauthSignature.generate(method, url, params, consumerSecret, tokenSecret, { encodeSignature: false});
-                        params['oauth_signature'] = signature;
-                        console.log(params);
-                        $http.jsonp(url, {params: params}).success(callback);
-    }
-
-  	}
+	
